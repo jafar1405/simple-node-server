9a8d36a5f37011363fa16612d6c21cf1a8133583
Jafar <jafar1405@gmail.com>
2019-03-10 16:56:35 +0530

    added heading with background color red and text color red

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-    res.end('hi')
+    res.end(`
+        <!doctype html>
+            <html>
+                <head>
+                    <title>Simple node server</title>
+                </head>
+                <body>
+                    <h1 style="background-color:red; color:white">Simple node server</h1>
+                </body>
+            </html>
+    
+    `)
 }).listen(process.env.PORT || 8080)
